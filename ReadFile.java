e229302326ae50758d0fa21e33b031513b2e039c
David Borges <david81br@gmail.com>
2019-09-07 12:21:03 -0300

    Track files

--- ReadFile.java
+++ ReadFile.java
@@ -1,32 +1,31 @@
 import java.io.*;
 
 public class ReadFile {
-    public static void main(String[] args){
-	int i;
-	FileInputStream fis;
-	char byes;
-	try {
-	    fis = new FileInputStream("read.txt");
-	    System.out.println("File found!");
-	} catch (FileNotFoundException fnfe){
-	    System.out.println("File Not Found");
-	    return;
-	}
-	try {
-	    do {
-		i = fis.read();
-		if (i!=-1) {
-		    byes = (char) i;
-		    System.out.print(byes);
+	public static void main(String[] args) {
+		int i;
+		FileInputStream fis;
+		char byes;
+		try {
+			fis = new FileInputStream("read.txt");
+			System.out.println("File found!");
+		} catch (FileNotFoundException fnfe) {
+			System.out.println("File Not Found");
+			return;
 		}
-		} while (i!=-1);
-	} catch (IOException ioe){
-	    System.out.println("EOF");
+		try {
+			do {
+				i = fis.read();
+				if (i != -1) {
+					byes = (char) i;
+					System.out.print(i + " " + byes + "\n");
+				}
+			} while (i != -1);
+		} catch (IOException ioe) {
+			System.out.println("EOF");
+		} try {
+			fis.close();
+		} catch (IOException ioe){
+			System.out.println("Can not close!");
+		}
 	}
-	try {
-	    fis.close();
-	} catch (IOException ioe){
-	    System.out.println("Error on file close");
-	}
-    }
 }
